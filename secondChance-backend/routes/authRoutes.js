c73db44dff588bec25ad70210218d3fbca537aed
Aissa Ouboukioud <aissa.1bacsm1@gmail.com>
2025-11-15 21:39:41 +0100

    format project

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,141 +1,156 @@
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const express = require('express');
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const express = require("express");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
-const connectToDatabase = require('../models/db');
-const pino = require('pino');
-const dotenv = require('dotenv');
+const { body, validationResult } = require("express-validator");
+const connectToDatabase = require("../models/db");
+const pino = require("pino");
+const dotenv = require("dotenv");
 
 const logger = pino();
 
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post('/register', async (req, res, next) => {
-    logger.info('/register called');
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("users");
+router.post("/register", async (req, res, next) => {
+  logger.info("/register called");
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
 
-        const { email, password, firstName, lastName } = req.body;
-        const existingEmail = await collection.findOne({ email: email });
-        if (existingEmail) {
-            logger.error('Email id already exists');
-            return res.status(400).json({ error: 'Email id already exists' });
-        }
-
-        const salt = await bcryptjs.genSalt(10);
-        const hash = await bcryptjs.hash(password, salt)
-
-        const newUser = await collection.insertOne({
-            email: email,
-            firstName: firstName,
-            lastName: lastName,
-            password: hash,
-            createdAt: new Date()
-        });
-
-        const payload = {
-            user: {
-                id: newUser.insertedId,
-            },
-        };
-
-        const authtoken = jwt.sign(payload, JWT_SECRET);
-        logger.info('User registered successfully');
-        
-        res.json({ authtoken,email });
-    } catch (e) {
-        logger.error(e);
-        return res.status(500).send('Internal server error');
+    const { email, password, firstName, lastName } = req.body;
+    const existingEmail = await collection.findOne({ email: email });
+    if (existingEmail) {
+      logger.error("Email id already exists");
+      return res.status(400).json({ error: "Email id already exists" });
     }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hash = await bcryptjs.hash(password, salt);
+
+    const newUser = await collection.insertOne({
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      password: hash,
+      createdAt: new Date(),
+    });
+
+    const payload = {
+      user: {
+        id: newUser.insertedId,
+      },
+    };
+
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+    logger.info("User registered successfully");
+
+    res.json({ authtoken, email });
+  } catch (e) {
+    logger.error(e);
+    return res.status(500).send("Internal server error");
+  }
 });
 
-router.post('/login', async (req, res) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("users");
+router.post("/login", async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
 
-        const { email } = req.body;
+    const { email } = req.body;
 
-        const theUser = await collection.findOne({ email: email });
+    const theUser = await collection.findOne({ email: email });
 
-        if (theUser) {
-            let result = await bcryptjs.compare(req.body.password, theUser.password)
-            if(!result) {
-                logger.error('Passwords do not match');
-                return res.status(404).json({ error: 'Wrong pasword' });
-            }
+    if (theUser) {
+      let result = await bcryptjs.compare(req.body.password, theUser.password);
+      if (!result) {
+        logger.error("Passwords do not match");
+        return res.status(404).json({ error: "Wrong pasword" });
+      }
 
-            const userName = theUser.firstName;
-            const userEmail = theUser.email;
+      const userName = theUser.firstName;
+      const userEmail = theUser.email;
 
-            let payload = {
-                user: {
-                    id: theUser._id.toString(),
-                },
-            };
-            const authtoken = jwt.sign(payload, JWT_SECRET);
-            logger.info('User logged in successfully');
-            return res.status(200).json({ authtoken, userName, userEmail });
-        } else {
-            logger.error('User not found');
-            return res.status(404).json({ error: 'User not found' });
-        }
-    } catch (e) {
-        logger.error(e);
-        return res.status(500).send('Internal server error');
+      let payload = {
+        user: {
+          id: theUser._id.toString(),
+        },
+      };
+      const authtoken = jwt.sign(payload, JWT_SECRET);
+      logger.info("User logged in successfully");
+      return res.status(200).json({ authtoken, userName, userEmail });
+    } else {
+      logger.error("User not found");
+      return res.status(404).json({ error: "User not found" });
     }
+  } catch (e) {
+    logger.error(e);
+    return res.status(500).send("Internal server error");
+  }
 });
 
-router.put('/update', [
-    body('firstName').optional().notEmpty().withMessage('First name cannot be empty'),
-    body('lastName').optional().notEmpty().withMessage('Last name cannot be empty'),
-    body('password').optional().notEmpty().withMessage('Password cannot be empty')
-], async (req, res) => {
+router.put(
+  "/update",
+  [
+    body("firstName")
+      .optional()
+      .notEmpty()
+      .withMessage("First name cannot be empty"),
+    body("lastName")
+      .optional()
+      .notEmpty()
+      .withMessage("Last name cannot be empty"),
+    body("password")
+      .optional()
+      .notEmpty()
+      .withMessage("Password cannot be empty"),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        logger.error('Validation errors in update request', errors.array());
-        return res.status(400).json({ errors: errors.array() });
+      logger.error("Validation errors in update request", errors.array());
+      return res.status(400).json({ errors: errors.array() });
     }
     try {
-        const email = req.headers.email;
-        if (!email) {
-            logger.error('Email not found in the request headers');
-            return res.status(400).json({ error: "Email not found in the request headers" });
-        }
-        
-        const db = await connectToDatabase();
-        const collection = db.collection("users");
-        
-        const existingUser = await collection.findOne({ email });
-        if (!existingUser) {
-            logger.error('User not found');
-            return res.status(404).json({ error: "User not found" });
-        }
-        existingUser.firstName = req.body.name;
-        existingUser.updatedAt = new Date();
-        
-        const updatedUser = await collection.findOneAndUpdate(
-            { email },
-            { $set: existingUser },
-            { returnDocument: 'after' }
-        );
-        
-        const payload = {
-            user: {
-                id: updatedUser._id.toString(),
-            },
-        };
-        const authtoken = jwt.sign(payload, JWT_SECRET);
-        logger.info('User updated successfully');
-        res.json({ authtoken });
+      const email = req.headers.email;
+      if (!email) {
+        logger.error("Email not found in the request headers");
+        return res
+          .status(400)
+          .json({ error: "Email not found in the request headers" });
+      }
+
+      const db = await connectToDatabase();
+      const collection = db.collection("users");
+
+      const existingUser = await collection.findOne({ email });
+      if (!existingUser) {
+        logger.error("User not found");
+        return res.status(404).json({ error: "User not found" });
+      }
+      existingUser.firstName = req.body.name;
+      existingUser.updatedAt = new Date();
+
+      const updatedUser = await collection.findOneAndUpdate(
+        { email },
+        { $set: existingUser },
+        { returnDocument: "after" },
+      );
+
+      const payload = {
+        user: {
+          id: updatedUser._id.toString(),
+        },
+      };
+      const authtoken = jwt.sign(payload, JWT_SECRET);
+      logger.info("User updated successfully");
+      res.json({ authtoken });
     } catch (error) {
-        logger.error(error);
-        return res.status(500).send("Internal Server Error");
+      logger.error(error);
+      return res.status(500).send("Internal Server Error");
     }
-});
+  },
+);
 
 module.exports = router;
--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 const router = express.Router();
-const connectToDatabase = require('../models/db');
-const logger = require('../logger');
+const connectToDatabase = require("../models/db");
+const logger = require("../logger");
 
 // Define the upload directory path
-const directoryPath = 'public/images';
+const directoryPath = "public/images";
 
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
@@ -21,88 +21,87 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-
 // Get all secondChanceItems
-router.get('/', async (req, res, next) => {
-    logger.info('/ called');
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
-        const secondChanceItems = await collection.find({}).toArray();
-        res.json(secondChanceItems);
-    } catch (e) {
-        logger.console.error('oops something went wrong', e)
-        next(e);
-    }
+router.get("/", async (req, res, next) => {
+  logger.info("/ called");
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection("secondChanceItems");
+    const secondChanceItems = await collection.find({}).toArray();
+    res.json(secondChanceItems);
+  } catch (e) {
+    logger.console.error("oops something went wrong", e);
+    next(e);
+  }
 });
 
 // Add a new item
-router.post('/', upload.single('file'),  async(req, res,next) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
-        const newItem = req.body;
-        if (req.file) {
-            newItem.image = req.file.filename;
-        }
-        const secondChanceItem = await collection.insertOne(newItem);
-        res.status(201).json(secondChanceItem.ops[0]);
-    } catch (e) {
-        next(e);
+router.post("/", upload.single("file"), async (req, res, next) => {
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection("secondChanceItems");
+    const newItem = req.body;
+    if (req.file) {
+      newItem.image = req.file.filename;
     }
+    const secondChanceItem = await collection.insertOne(newItem);
+    res.status(201).json(secondChanceItem.ops[0]);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Get a single secondChanceItem by ID
-router.get('/:id', async (req, res, next) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
-        const id = req.params.id;
-        const secondChanceItem = await collection.findOne({ id: id });
-        if (!secondChanceItem) {
-            return res.status(404).json({ message: "Item not found" });
-        }
-        res.json(secondChanceItem);
-    } catch (e) {
-        next(e);
+router.get("/:id", async (req, res, next) => {
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection("secondChanceItems");
+    const id = req.params.id;
+    const secondChanceItem = await collection.findOne({ id: id });
+    if (!secondChanceItem) {
+      return res.status(404).json({ message: "Item not found" });
     }
+    res.json(secondChanceItem);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Update and existing item
-router.put('/:id', async(req, res,next) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
-        const id = req.params.id;
-        const updateItem = req.body;
-        const secondChanceItem = await collection.findOneAndUpdate(
-            { id: id },
-            { $set: updateItem },
-            { returnDocument: "after" }
-        );
-        if (secondChanceItem.matchedCount === 0) {
-            return res.status(404).json({ message: "Item not found" });
-        }
-        res.json({ id: id, message: 'Item has been updated successfully' });
-    } catch (e) {
-        next(e);
+router.put("/:id", async (req, res, next) => {
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection("secondChanceItems");
+    const id = req.params.id;
+    const updateItem = req.body;
+    const secondChanceItem = await collection.findOneAndUpdate(
+      { id: id },
+      { $set: updateItem },
+      { returnDocument: "after" },
+    );
+    if (secondChanceItem.matchedCount === 0) {
+      return res.status(404).json({ message: "Item not found" });
     }
+    res.json({ id: id, message: "Item has been updated successfully" });
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Delete an existing item
-router.delete('/:id', async(req, res,next) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
-        const id = req.params.id;
-        const result = await collection.deleteOne({ id: id });
-        if (result.deletedCount === 0) {
-            return res.status(404).json({ message: "Item not found" });
-        }
-        res.json({ id: id, message: 'Item has been deleted successfully' });
-    } catch (e) {
-        next(e);
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection("secondChanceItems");
+    const id = req.params.id;
+    const result = await collection.deleteOne({ id: id });
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "Item not found" });
     }
+    res.json({ id: id, message: "Item has been deleted successfully" });
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
